a554963a10e1d2651abac02e068382ccd5db8414
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-11-07 10:08:15 +0100

    simple brute force

--- ShaCracker/PasswordGenerator.cs
+++ ShaCracker/PasswordGenerator.cs
@@ -1,6 +1,98 @@
 ﻿namespace ShaCracker;
 
-public class PasswordGenerator
+public static class PasswordGenerator
 {
-    
+    public static char[] Chars =
+    {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z',
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'i',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        'r',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z'
+    };
+
+    public static IEnumerable<(char first, char second)> Produce2Chars()
+    {
+        foreach (var first in Chars)
+        {
+            foreach (var second in Chars)
+            {
+                yield return (first, second);
+            }
+        }
+    }
+
+    public static IEnumerable<int> ProducePasswords(char first, char second, byte[] buffer)
+    {
+        if (buffer.Length != 6) throw new ArgumentException("buffer must have length of 6");
+        
+        buffer[0] = (byte) first;
+        buffer[1] = (byte) second;
+
+        foreach (var third in Chars)
+        {
+            foreach (var fourth in Chars)
+            {
+                foreach (var fifth in Chars)
+                {
+                    foreach (var sixth in Chars)
+                    {
+                        buffer[2] = (byte) third;
+                        buffer[3] = (byte) fourth;
+                        buffer[4] = (byte) fifth;
+                        buffer[5] = (byte) sixth;
+                        yield return 1;
+                    }
+                }
+            }
+        }
+    }
 }
--- ShaCracker/Program.cs
+++ ShaCracker/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ShaCracker;
+
+public static class Program
+{
+    private const int HashSizeBits = 160;
+    private const int HashSizeBytes = HashSizeBits / 8;
+
+    public static int Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Needs a hash -> ./ShaCracker.exe hash");
+            return 1;
+        }
+
+        var shouldBe = args[0];
+            var shouldBeBytes = (from x in Enumerable.Range(0, shouldBe.Length)
+                where x % 2 == 0
+                select Convert.ToByte(shouldBe.Substring(x, 2), 16)).ToArray();
+
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        Parallel.ForEach(PasswordGenerator.Produce2Chars(), (c, state) =>
+        {
+            var passwordBuffer = GC.AllocateUninitializedArray<byte>(6);
+            var hashBuffer = (Span<byte>) GC.AllocateUninitializedArray<byte>(HashSizeBytes);
+
+            foreach (var dummy in PasswordGenerator.ProducePasswords(c.first, c.second, passwordBuffer))
+            {
+                SHA1.HashData(passwordBuffer, hashBuffer);
+                if (!passwordBuffer.SequenceEqual(shouldBeBytes)) continue;
+
+                Console.WriteLine($"Found password {Encoding.ASCII.GetString(passwordBuffer)}");
+                Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
+                state.Stop();
+                break;
+            }
+        });
+
+        return 0;
+    }
+}
