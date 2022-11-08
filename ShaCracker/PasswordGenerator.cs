e8b9ec402e59f23d66e92e6c49826525e307b203
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-11-08 20:22:32 +0100

    comment code

--- ShaCracker/PasswordGenerator.cs
+++ ShaCracker/PasswordGenerator.cs
@@ -2,6 +2,9 @@
 
 public static class PasswordGenerator
 {
+    /// <summary>
+    /// All characters that can be used in the password
+    /// </summary>
     public static char[] Chars =
     {
         'A',
@@ -59,6 +62,12 @@ public static class PasswordGenerator
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     };
 
+    /// <summary>
+    /// Produces a sequence of all possible 2 char arrays.
+    /// </summary>
+    /// <param name="startFirst">First start index</param>
+    /// <param name="startSecond">Second start index</param>
+    /// <returns>Sequence of all possible 2 char arrays.</returns>
     public static IEnumerable<(char first, char second)> Produce2Chars(int startFirst = 0, int startSecond = 0)
     {
         for (var i = startFirst; i < Chars.Length; i++)
@@ -72,10 +81,15 @@ public static class PasswordGenerator
         }
     }
 
+    /// <summary>
+    /// Create a sequence of all possible char combinations with length 6 with 2 starting characters.
+    /// </summary>
+    /// <param name="first">The first <see cref="char"/> to be put into the buffer.</param>
+    /// <param name="second">The second <see cref="char"/> to be put into the buffer.</param>
+    /// <param name="buffer">Byte array to be written into.</param>
+    /// <returns>Dummy value, always 1</returns>
     public static IEnumerable<int> ProducePasswords(char first, char second, byte[] buffer)
     {
-        if (buffer.Length != 6) throw new ArgumentException("buffer must have length of 6");
-
         buffer[0] = (byte)first;
         buffer[1] = (byte)second;
 
--- ShaCracker/Program.cs
+++ ShaCracker/Program.cs
@@ -17,25 +17,33 @@ public static class Program
             return 1;
         }
 
+        // the first arg should be the hash
         var shouldBe = args[0];
+        // convert the given hash into a byte array
         var shouldBeBytes = (from x in Enumerable.Range(0, shouldBe.Length)
             where x % 2 == 0
             select Convert.ToByte(shouldBe.Substring(x, 2), 16)).ToArray();
 
+        // start time
         var stopwatch = new Stopwatch();
         stopwatch.Start();
 
+        // start a thread for each 2 character combination
         Parallel.ForEach(PasswordGenerator.Produce2Chars(), (c, state) =>
         {
             Console.WriteLine($"Starting Thread {Environment.CurrentManagedThreadId} trying {c.first}{c.second}????");
+            // allocate memory
             var passwordBuffer = GC.AllocateUninitializedArray<byte>(6);
             var hashBuffer = (Span<byte>) GC.AllocateUninitializedArray<byte>(HashSizeBytes);
 
+            // generate 4 more characters to test all 6 character sequences
+            // and check if the SHA1 hash matches with the given hash from above
             foreach (var dummy in PasswordGenerator.ProducePasswords(c.first, c.second, passwordBuffer))
             {
                 SHA1.HashData(passwordBuffer, hashBuffer);
                 if (!hashBuffer.SequenceEqual(shouldBeBytes)) continue;
 
+                // print out the password and kill the parallel operation
                 Console.WriteLine($"Found password {Encoding.ASCII.GetString(passwordBuffer)}");
                 Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
                 state.Stop();
