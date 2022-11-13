dfa7eb21af01072541b9c8add3561dde5fb2f0d8
Kevin T <30321316+glaciyan@users.noreply.github.com>
2022-11-13 11:04:38 +0100

    replace iterator with for loop

--- ShaCracker/Program.cs
+++ ShaCracker/Program.cs
@@ -35,29 +35,47 @@ public static class Program
             // allocate memory
             var passwordBuffer = GC.AllocateUninitializedArray<byte>(6);
             var hashBuffer = GC.AllocateUninitializedArray<byte>(HashSizeBytes);
+            
+            passwordBuffer[0] = (byte) c.first;
+            passwordBuffer[1] = (byte) c.second;
 
             // generate 4 more characters to test all 6 character sequences
             // and check if the SHA1 hash matches with the given hash from above
-            foreach (var dummy in PasswordGenerator.ProducePasswords(c.first, c.second, passwordBuffer))
+            foreach (var third in PasswordGenerator.Chars)
             {
-                SHA1.HashData(passwordBuffer, hashBuffer);
-                var equal = true;
-                for (var i = 0; i < HashSizeBytes; i++)
+                foreach (var fourth in PasswordGenerator.Chars)
                 {
-                    if (hashBuffer[i] != shouldBeBytes[i])
+                    foreach (var fifth in PasswordGenerator.Chars)
                     {
-                        equal = false;
-                        break;
+                        foreach (var sixth in PasswordGenerator.Chars)
+                        {
+                            passwordBuffer[2] = (byte) third;
+                            passwordBuffer[3] = (byte) fourth;
+                            passwordBuffer[4] = (byte) fifth;
+                            passwordBuffer[5] = (byte) sixth;
+                            
+                            SHA1.HashData(passwordBuffer, hashBuffer);
+                            var equal = true;
+                            for (var i = 0; i < HashSizeBytes; i++)
+                            {
+                                if (hashBuffer[i] != shouldBeBytes[i])
+                                {
+                                    equal = false;
+                                    break;
+                                }
+                            }
+
+                            if (!equal) continue;
+
+                            // print out the password and kill the parallel operation
+                            Console.WriteLine($"Found password {Encoding.ASCII.GetString(passwordBuffer)}");
+                            Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
+                            // state.Stop();
+                            Environment.Exit(0);
+                            break;
+                        }
                     }
                 }
-                if (!equal) continue;
-
-                // print out the password and kill the parallel operation
-                Console.WriteLine($"Found password {Encoding.ASCII.GetString(passwordBuffer)}");
-                Console.WriteLine($"Time elapsed: {stopwatch.Elapsed}");
-                // state.Stop();
-                Environment.Exit(0);
-                break;
             }
         });
 
